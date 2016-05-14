a6a4bfb5ea22dd88856d97c5859d2cb7074f84b6
Pouli <paul.deblicker@isen-lille.fr>
2016-05-14 16:45:56 +0200

    [NAVBAR] filter modification for current user display

--- imports/app.filter.js
+++ imports/app.filter.js
@@ -2,8 +2,10 @@ export const displayNameFilter = () => {
     return (user) => {
         if (!user) return '';
         if (user.profile && user.profile.name) return user.profile.name;
-        if (user.emails) return user.emails[0].address;
-
+        if (user.emails) {
+            const email = user.emails[0].address;
+            return email.substring(0, email.lastIndexOf('@'));
+        }
         return user;
     }
 };
